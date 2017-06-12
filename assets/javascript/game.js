bd4655e4379e19077206863d9986c396a7b3318b
Joshua Serrano-Patron <jrsp@Joshuas-MacBook-Pro.local>
2017-06-12 15:14:33 -0400

    Working Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,34 +14,60 @@ function getRandomInt(min, max) {
 	};
 
 function crystalValue(){
-  for (var i = 0; i < images.length; i++) {
-    var assign = getRandomInt(1,12);
-    var crystalImage = $("<img>");
-    crystalImage.addClass("crystals");
-    crystalImage.attr("src", images[i]);
-    crystalImage.attr("data-crystalvalue",assign);
-    $("#crystalImages").append(crystalImage);
-  }
+
+      $("#crystalImages").html("");
+
+        for (var i = 0; i < images.length; i++) {
+          var assign = getRandomInt(1,12);
+          var crystalImage = $("<img>");
+          crystalImage.addClass("crystals");
+          crystalImage.attr("src", images[i]);
+          crystalImage.attr("data-crystalvalue",assign);
+          $("#crystalImages").append(crystalImage);
+        }
 };
 
 crystalValue();
 
-$(document).on("click", ".crystals", function() {
-  var number = $(this).data("crystalvalue");
-  console.log(number);
-  yourNumber += parseInt(number);
 
-console.log(randomNumber);
-console.log(yourNumber);
-if (yourNumber == randomNumber){
-  alert("You Win");
-  wins++;
-}
+function playAgain(){
+    crystalValue();
+    randomNumber = getRandomInt(19,120);
+    $("#currentNumber").html(randomNumber);
+    yourNumber = 0;
+    $("#your").html("Your total score is: " + yourNumber);
+};
+
+
+$("#wins").html("Wins: " + wins);
+$("#loses").html("Loses: " + loses);
+
+
+
+$(document).on("click", ".crystals", function() {
+      var number = $(this).data("crystalvalue");
+      console.log(number);
+      yourNumber += parseInt(number);
+
+    console.log(randomNumber);
+    console.log(yourNumber);
+    if (yourNumber == randomNumber){
+      alert("You Win!");
+      wins++;
+      $("#wins").html("Wins: " + wins);
+      playAgain();
+    }
+    else if (yourNumber > randomNumber){
+      alert("You Lose!");
+      loses++;
+      $("#loses").html("Loses: " + loses);
+      playAgain();
+    }
   //if yourNumber > goal => you lose update losses
 
   //reset the game => set yourNumber back to 0 and get a new random number
 
-  $("#your").html("Your total score is:" + yourNumber);
+      $("#your").html("Your total score is: " + yourNumber);
 });
 
 randomNumber = getRandomInt(19,120);
